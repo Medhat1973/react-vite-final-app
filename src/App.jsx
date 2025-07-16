2b56f2352d2495fff4367743fbd9106c85216622
Kareem_Medhat <kareem.medhat1804@gmail.com>
2025-07-16 15:01:59 +0300

    update for the app.jsx file
    i added a comment to this fille

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Posts from './Posts';
 import Users from './Users';
-
+// hello thhis is kareem
 function App() {
   return (
     <div style={{ padding: '20px', fontFamily: 'Arial' }}>
